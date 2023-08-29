d26c45eee6e50bfa8a5624e1171080f193d82bbb
Saurabh Rai <saurabhrai.cec@gmail.com>
2023-08-29 23:21:26 +0530

    tic tac toe board component

---
+++ src/components/TicTacToe/Board.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Box } from './Box';
+
+import './style/Board.css';
+
+const Board = ({ board, onClick }) => {
+  return (
+    <div className='board'>
+        {
+            board.map((value, idx) => {
+                return <Box key={idx} value={value} onClick={() => value === null && onClick(idx)}/>
+            })
+        }
+    </div>
+  )
+}
+
+export default Board
