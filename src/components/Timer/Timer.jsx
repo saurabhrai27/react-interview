17ed6379b2fbf09c120e8175258f269da9264189
Saurabh Rai <saurabhrai.cec@gmail.com>
2023-10-27 22:57:42 +0530

    added timer component

---
+++ src/components/Timer/Timer.jsx
@@ -0,0 +1,56 @@
+import { useState, useEffect } from 'react';
+
+const Timer = () => {
+  const initialTime = 5000; // Initial time in milliseconds
+  const [time, setTime] = useState(initialTime);
+  const [isRunning, setIsRunning] = useState(false);
+
+  useEffect(() => {
+    let timer;
+    if (isRunning) {
+      timer = setInterval(() => {
+        setTime((prevTime) => {
+          if (prevTime === 0) {
+            clearInterval(timer);
+            setIsRunning(false);
+          }
+          return prevTime - 1000;
+        });
+      }, 1000);
+    }
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, [isRunning]);
+
+  const handleStart = () => {
+    setIsRunning(true);
+  };
+
+  const handleStop = () => {
+    setIsRunning(false);
+  };
+
+  const handleReset = () => {
+    setTime(initialTime);
+    setIsRunning(false);
+  };
+
+  const formatTime = (milliseconds) => {
+    const minutes = Math.floor(milliseconds / 60000);
+    const seconds = Math.floor((milliseconds % 60000) / 1000);
+    return `${minutes}:${seconds.toString().padStart(2, '0')}`;
+  };
+
+  return (
+    <div>
+      <div>Time: {formatTime(time)}</div>
+      <button onClick={handleStart}>Start</button>
+      <button onClick={handleStop}>Stop</button>
+      <button onClick={handleReset}>Reset</button>
+    </div>
+  );
+};
+
+export default Timer;
