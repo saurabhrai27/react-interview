8f2cbf58cc6bb58f5bcb283c68f35b0e3de1fc0e
Saurabh Rai <saurabhrai.cec@gmail.com>
2023-10-30 23:57:43 +0530

    added traffic light component structure

---
+++ src/components/TrafficLights/TrafficLights.jsx
@@ -0,0 +1,12 @@
+import { useState, useEffect } from 'react';
+
+const TrafficLights = () => {  
+return (
+    <div>
+      <h2>TrafficLights</h2>
+      
+    </div>
+  );
+};
+
+export default TrafficLights;
