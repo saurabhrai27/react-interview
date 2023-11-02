14417306fc69e74b38fe0ce9f2577bc2b7b22b60
Saurabh Rai <saurabhrai.cec@gmail.com>
2023-11-02 23:13:09 +0530

    added traffic light component

--- src/components/TrafficLights/TrafficLights.jsx
+++ src/components/TrafficLights/TrafficLights.jsx
@@ -1,10 +1,28 @@
 import { useState, useEffect } from 'react';
+import './style/TrafficLights.css';
 
-const TrafficLights = () => {  
-return (
-    <div>
-      <h2>TrafficLights</h2>
-      
+const TrafficLights = () => {
+  const [activeColor, setActiveColor] = useState('red');
+
+  useEffect(() => {
+    const colors = ['red', 'yellow', 'green'];
+    let currentIndex = colors.indexOf(activeColor);
+
+    const interval = setInterval(() => {
+      currentIndex = (currentIndex + 1) % colors.length;
+      setActiveColor(colors[currentIndex]);
+    }, 2000); // Change color every 2 seconds
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [activeColor]);
+
+  return (
+    <div className="traffic-light">
+      <div className={`light red ${activeColor === 'red' ? 'active' : ''}`}></div>
+      <div className={`light yellow ${activeColor === 'yellow' ? 'active' : ''}`}></div>
+      <div className={`light green ${activeColor === 'green' ? 'active' : ''}`}></div>
     </div>
   );
 };
